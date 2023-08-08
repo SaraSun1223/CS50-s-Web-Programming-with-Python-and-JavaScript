425f0670c58eaebfd7d2870c137fa5bf36454a1b
SaraSun1223 <jp2019213204@qmul.ac.uk>
2023-08-08 18:26:19 +0800

    8.8

--- Lecture3/lecture3/hello/urls.py
+++ Lecture3/lecture3/hello/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("sara", views.sara, name = "sara")
+    path("sara", views.sara, name = "sara"),
+    path("<str:name>", views.greet, name = "greet")
 ]
--- Lecture3/lecture3/hello/views.py
+++ Lecture3/lecture3/hello/views.py
@@ -3,8 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello World!")
+
+# Add index.html in templates/hello
 def index(request):
-    return HttpResponse("Hello World!")
+    return render(request, "hello/index.html")
 
 def sara(request):
     return HttpResponse("Hello, Sara!")
+
+# 
+# def greet(request,name):
+#     return HttpResponse(f"Hello, {name.capitalize()}!")
+
+# Add greet.html in templates/hello
+def greet(request,name):
+    return render(request,"hello/greet.html",{
+        "name": name.capitalize()
+    })
